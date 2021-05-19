19c4872cf3c27a328e299c0256736815feddc1c4
Thoriqul Umar <bang.thor.tu@gmail.com>
2021-05-19 20:00:30 +0700

    give ML model test on video

--- Android/FireDetector/app/src/main/java/com/thor/firedetector/MainActivity.kt
+++ Android/FireDetector/app/src/main/java/com/thor/firedetector/MainActivity.kt
@@ -1,11 +1,98 @@
 package com.thor.firedetector
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Rational
+import android.util.Size
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.impl.ImageAnalysisConfig
+import androidx.camera.core.impl.PreviewConfig
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_PERMISSIONS = 101
+    private val REQUIRED_PERMISSIONS = arrayOf("android.permission.CAMERA")
+    private var lensFacing = CameraX.LensFacing.BACK
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                        .show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                            this,
+                            permission
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun startCamera() {
+        val metrics = DisplayMetrics().also { texture.display.getRealMetrics(it) }
+        val screenSize = Size(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = Rational(1, 1)
+
+        val previewConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetResolution(screenSize)
+            setTargetAspectRatio(screenAspectRatio)
+            setTargetRotation(windowManager.defaultDisplay.rotation)
+            setTargetRotation(textureView.display.rotation)
+        }.build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            textureView.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // Use a worker thread for image analysis to prevent glitches
+            val analyzerThread = HandlerThread("AnalysisThread").apply {
+                start()
+            }
+            setCallbackHandler(Handler(analyzerThread.looper))
+            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+
+        val analyzerUseCase = ImageAnalysis(analyzerConfig)
+        analyzerUseCase.analyzer =
+                ImageAnalysis.Analyzer { image: ImageProxy, rotationDegrees: Int ->
+
+                    val bitmap = image.toBitmap()
+
+                    tfLiteClassifier
+                            .classifyAsync(bitmap)
+                            .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
+                            .addOnFailureListener { error -> }
+
+                }
+        CameraX.bindToLifecycle(this, preview, analyzerUseCase)
+    }
+
 }
